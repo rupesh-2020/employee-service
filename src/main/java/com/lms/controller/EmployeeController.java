6097e7a54188d998a8ade9ab064d9d52927c6732
unknown <rupesh.bhuwad@yahoo.com>
2020-07-12 10:34:29 +0530

    second commit

--- src/main/java/com/lms/controller/EmployeeController.java
+++ src/main/java/com/lms/controller/EmployeeController.java
@@ -2,7 +2,9 @@ package com.lms.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lms.entity.Employee;
@@ -11,8 +13,10 @@ import com.lms.util.DBUtil;
 @RestController
 public class EmployeeController {
 
-	@RequestMapping(value="/employwwa")
-	public List<Employee> getEmployeeList(){
-		return DBUtil.generateEmployeeList();
+	@GetMapping("/employees")
+	public ResponseEntity<List<Employee>> getEmployeeList(){
+		List<Employee> list = DBUtil.generateEmployeeList();
+		ResponseEntity<List<Employee>> entity = new ResponseEntity<List<Employee>>(list,HttpStatus.OK);
+		return entity;
 	}
 }
